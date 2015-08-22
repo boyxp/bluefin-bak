2d9222e6b7cc128cafd7c23b6d35c0de9db06c1c
bluefin-framework <isme@live.com>
2015-08-22 23:03:51 +0800

    接口方法调整

--- bluefin/component/router.php
+++ bluefin/component/router.php
@@ -2,11 +2,9 @@
 namespace component;
 interface router
 {
-	public function get($rule, callable $handle);
-	public function post($rule, callable $handle);
-	public function put($rule, callable $handle);
-	public function delete($rule, callable $handle);
-	public function route($uri=null);
+	public function addRule($method, $pattern, callable $handle);
+	public function removeRule($method, $pattern);
+	public function route(\component\request $request=null);
 	public function getHandle();
 	public function getParams();
 }
