65e12f4b6c847d0660b039d47531aed4d2c4882c
bluefin-framework <isme@live.com>
2015-08-29 23:04:45 +0800

    query

---
+++ bluefin/library/orm/query.php
@@ -0,0 +1,13 @@
+<?php
+namespace library\orm;
+interface query
+{
+	public function columns($columns='*');
+	public function from();
+	public function where($condition, array $bind=null);
+	public function group($fields);
+	public function having($condition, array $bind=null);
+	public function order($field, $direction='ASC');
+	public function limit($count=20, $offset=0);
+	public function fetch($all=true);
+}
