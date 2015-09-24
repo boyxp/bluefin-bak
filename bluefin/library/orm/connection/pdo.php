17886c1211c20f0f043f7efb6bcfc061181905d7
bluefin-framework <isme@live.com>
2015-09-24 22:55:00 +0800

    dsn编码设置

--- bluefin/library/orm/connection/pdo.php
+++ bluefin/library/orm/connection/pdo.php
@@ -4,30 +4,26 @@ class pdo extends \PDO implements \library\orm\connection
 {
 	public function __construct($dsn, $user, $password, array $options=null)
 	{
+		if(strpos(strtolower($dsn), 'charset=')!==false) {
+			preg_match('/charset=([a-z0-9-]+)/i', $dsn, $match);
+			$charset = isset($match[1]) ? $match[1] : 'utf8';
+		} else {
+			$charset = isset($options['charset']) ? $options['charset'] : 'utf8';
+			$dsn    .= (substr($dsn, -1)===';' ? '' : ';')."charset={$charset}";
+		}
+
 		try {
-			parent::__construct($dsn, $user, $password, array(PDO::ATTR_PERSISTENT => true));
+			parent::__construct($dsn, $user, $password, array(\PDO::ATTR_PERSISTENT => true));
 		} catch (exception $e) {
 			throw new \exception('Connection failed: '.$e->getMessage(), $e->getCode());
 		}
 
-		//attribute
-		$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		$this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
-		//charset
-		if(isset($options['charset'])) {
-			$charset = $options['charset'];
-		} elseif(strpos($dsn, 'charset=')!==false) {
-			preg_match('/charset=([a-z0-9-]+)/i', $dsn, $match);
-			$charset = isset($match[1]) ? $match[1] : 'utf8';
-		} else {
-			$charset = 'utf8';
-		}
-		$this->exec("SET NAMES '{$charset}'");
-
-		//timezone
 		$timezone = isset($options['timezone']) ? $options['timezone'] : '+08:00';
 		$this->exec("SET time_zone='{$timezone}'");
+		$this->exec("SET NAMES '{$charset}'");
 	}
 
 	public function begin()
