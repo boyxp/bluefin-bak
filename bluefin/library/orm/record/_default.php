0ce25234f4d575b11a45d7d04309d396117f359b
bluefin-framework <isme@live.com>
2015-10-15 23:13:23 +0800

    使用query更新记录

--- bluefin/library/orm/record/_default.php
+++ bluefin/library/orm/record/_default.php
@@ -2,28 +2,28 @@
 namespace library\orm\record;
 class _default implements \library\orm\record
 {
-	private $_table   = null;
-	private $_data    = null;
-	private $_key     = null;
-	private $_created = false;
+		private $_data    = null;
+		private $_query   = null;
+		private $_key     = null;
+		private $_created = false;
 
-	public function __construct($table, array $data=null, $key=null)
-	{
-		$this->_table   = $table;
-		$this->_key     = $key;
-		$this->_data    = $data===null ? array() : $data;
-		$this->_created = $data===null;
-	}
+		public function __construct(array $data=null, $query=null, $key=null)
+		{
+			$this->_data    = $data===null ? array() : $data;
+			$this->_query   = $query;
+			$this->_key     = $key;
+			$this->_created = $data===null;
+		}
 
-	public function __get($column)
-	{
-		return isset($this->_data[$column]) ? $this->_data[$column] : null;
-	}
+		public function __get($column)
+		{
+			return isset($this->_data[$column]) ? $this->_data[$column] : null;
+		}
 
-	public function __set($column, $value)
-	{
-		$this->_data[$column] = $value;
-	}
+		public function __set($column, $value)
+		{
+			$this->_data[$column] = $value;
+		}
 
 	public function save()
 	{
@@ -31,10 +31,8 @@ class _default implements \library\orm\record
 			$this->_key     = $this->_table->insert($this->_data);
 			$this->_created = false;
 		} else {
-			$this->_table->update($this->_data, $this->_key);
+			$this->_query->update($this->_data)->execute();
 		}
-
-		return $this->_key;
 	}
 
 	public function delete()
@@ -47,60 +45,60 @@ class _default implements \library\orm\record
 		}
 	}
 
-	//Countable
-	public function count()
-	{
-		return count($this->_data);
-	}
-
-
-	//Iterator
-	public function current()
-	{
-		return current($this->_data);
-	}
-
-	public function key()
-	{
-		return key($this->_data);
-	}
-
-	public function next()
-	{
-		next($this->_data);
-	}
-
-	public function rewind()
-	{
-		reset($this->_data);
-	}
-
-	public function valid()
-	{
-		return current($this->_data)!==false;
-	}
-
-
-	//ArrayAccess
-	public function offsetSet($key, $value)
-	{
-		if(!is_null($key)) {
-			$this->_data[$key] = $value;
+		//Countable
+		public function count()
+		{
+			return count($this->_data);
 		}
-	}
 
-	public function offsetExists($key)
-	{
-		return isset($this->_data[$key]);
-	}
 
-	public function offsetUnset($key)
-	{
-		unset($this->_data[$key]);
-	}
+		//Iterator
+		public function current()
+		{
+			return current($this->_data);
+		}
 
-	public function offsetGet($key)
-	{
-		return isset($this->_data[$key]) ? $this->_data[$key] : null;
-	}
+		public function key()
+		{
+			return key($this->_data);
+		}
+
+		public function next()
+		{
+			next($this->_data);
+		}
+
+		public function rewind()
+		{
+			reset($this->_data);
+		}
+
+		public function valid()
+		{
+			return current($this->_data)!==false;
+		}
+
+
+		//ArrayAccess
+		public function offsetSet($key, $value)
+		{
+			if(!is_null($key)) {
+				$this->_data[$key] = $value;
+			}
+		}
+
+		public function offsetExists($key)
+		{
+			return isset($this->_data[$key]);
+		}
+
+		public function offsetUnset($key)
+		{
+			unset($this->_data[$key]);
+		}
+
+		public function offsetGet($key)
+		{
+			return isset($this->_data[$key]) ? $this->_data[$key] : null;
+		}
 }
