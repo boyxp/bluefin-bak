1264b19c07a4e54d01b95d1dfe46f8e0ec91c69e
bluefin-framework <isme@live.com>
2015-10-19 22:53:48 +0800

    增加输出方法和资源获取方法

--- bluefin/library/image/handle.php
+++ bluefin/library/image/handle.php
@@ -2,11 +2,13 @@
 namespace library\image;
 interface handle
 {
-	public function getWidth();
+	public function getWidth();//首次打开用getimagesize取，资源创建后用imagesx\imagesy取
 	public function getHeight();
 	public function getMime();
 	public function getExtension();
 	public function getFilesize();
 	public function getDirection();
+	public function getResource();
+	public function output($filename=null);
 	public function __get($key);
 }
