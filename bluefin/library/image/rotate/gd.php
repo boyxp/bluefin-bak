57161cf0eb2b178abfb479e89c66e91b6fbd5ce1
bluefin-framework <isme@live.com>
2015-10-31 00:19:28 +0800

    处理完毕返回resource

--- bluefin/library/image/rotate/gd.php
+++ bluefin/library/image/rotate/gd.php
@@ -5,21 +5,20 @@ class gd implements \library\image\rotate
 	public function rotate(\library\image\handle $handle, $angle='auto', $background='#000000')
 	{
 		$background = hexdec(ltrim($background, '#'));
+		$resource   = $handle->getResource();
+
 		if($angle==='auto') {
 			$orientation = $handle->getOrientation();
 			switch($orientation) {
 				case 3: $angle = 180; break;
 				case 6: $angle = -90; break;
 				case 8: $angle = 90;  break;
-				default : return;
+				default : return $resource;
 			}
 		} else {
 			$angle = intval($angle);
 		}
 
-		$resource = $handle->getResource();
-		$resource = imagerotate($resource, $angle, $background);
-header("Content-type: {$handle->mime}");
-imagepng($resource);
+		return imagerotate($resource, $angle, $background);
 	}
 }
