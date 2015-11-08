5fd43a27ca19a6a1f172c007b89f19100f31e642
bluefin-framework <isme@live.com>
2015-11-08 22:23:43 +0800

    直接返回MongoDB实例，而不是MongoClient实例，以此限定数据库

--- bluefin/library/orm/connection/mongo.php
+++ bluefin/library/orm/connection/mongo.php
@@ -1,7 +1,9 @@
 <?php
 namespace library\orm\connection;
-class mongo extends \MongoClient implements \library\orm\connection
+class mongo extends \MongoDB implements \library\orm\connection
 {
+	protected static $_connection = array();
+
 	public function __construct($dsn, $user='', $password='', array $options=null)
 	{
 		$options = $options===null ? array('connect'=>true) : $options;
@@ -14,9 +16,14 @@ class mongo extends \MongoClient implements \library\orm\connection
 		}
 
 		$options['connectTimeoutMS'] = isset($options['connectTimeoutMS']) ? $options['connectTimeoutMS'] : 50;
+		$options['db']               = isset($options['db']) ? $options['db'] : 'bluefin';
 
 		try {
-			parent::__construct($dsn, $options);
+			if(!isset(static::$_connection[$dsn])) {
+				static::$_connection[$dsn] = new \MongoClient($dsn, $options);
+			}
+
+			parent::__construct(static::$_connection[$dsn], $options['db']);
 		} catch (exception $e) {
 			throw new \exception('Connection failed: '.$e->getMessage(), $e->getCode());
 		}
