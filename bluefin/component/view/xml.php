eafce7ac991f5fea579f890db15d0cd38e4ca8e9
bluefin-framework <isme@live.com>
2015-12-02 22:00:34 +0800

    view接口xml实现

---
+++ bluefin/component/view/xml.php
@@ -0,0 +1,44 @@
+<?php
+namespace component\view;
+class xml implements \component\view
+{
+	private $_charset = null;
+	private $_xml     = null;
+
+	public function __construct($charset='utf-8')
+	{
+		$this->_charset = $charset;
+	}
+
+	public function render(array $params=null)
+	{
+		header("Content-type: text/xml; charset={$this->_charset}");
+
+		$data = count($params)>1 ? array('root'=>$params) : $params;
+
+		$this->_xml = new \XMLWriter;
+		$this->_xml->openMemory();
+		$this->_xml->startDocument('1.0', $this->_charset);
+
+		$this->_build($data);
+
+		$this->_xml->endDocument();
+		echo $this->_xml->outputMemory(true);
+		exit;
+	}
+
+	private function _build($data, $node='node')
+	{
+		foreach($data as $key=>$value) {
+			$key = is_numeric($key) ? 'item' : $key;
+
+			$this->_xml->startElement($key);
+			if(is_array($value)) {
+				$this->{__FUNCTION__}($value, $key);
+			} else {
+				$this->_xml->text($value);
+			}
+			$this->_xml->endElement();
+		}
+	}
+}
