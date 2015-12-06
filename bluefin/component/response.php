bd12f6117c5a47b2b8b852a05007cc08770d85b3
bluefin-framework <isme@live.com>
2015-12-06 22:23:31 +0800

    不强制实现construct

--- bluefin/component/response.php
+++ bluefin/component/response.php
@@ -2,7 +2,6 @@
 namespace component;
 interface response
 {
-	public function __construct($body='', $status=200, array $headers=array());
 	public function setStatus($status);
 	public function setHeader($name, $value);
 	public function setHeaders(array $headers);
