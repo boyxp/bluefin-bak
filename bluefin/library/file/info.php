c411fda5fb08724d81d9d9e265222a0be7c919ed
bluefin-framework <isme@live.com>
2015-12-14 22:17:41 +0800

    增加读取文件hash方法

--- bluefin/library/file/info.php
+++ bluefin/library/file/info.php
@@ -4,10 +4,11 @@ interface info
 {
 	public function getSize($readable=false);
 	public function getMime();
-	public function getName();
+	public function getName($base=true);
 	public function getPath();
 	public function getExtension();
 	public function getModifyTime();
 	public function getAccessTime();
+	public function getHash($algo='md5');
 	public function __get($item);
 }
