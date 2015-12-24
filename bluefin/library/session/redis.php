21d8ce97f622f5473313b86877e41796af93824e
bluefin-framework <isme@live.com>
2015-12-25 00:07:49 +0800

    domain取值改为SERVER_NAME

--- bluefin/library/session/redis.php
+++ bluefin/library/session/redis.php
@@ -15,8 +15,8 @@ class redis implements \library\session
 	{
 		$this->_redis  = $redis;
 		$this->_secure = isset($_SERVER['HTTPS']);
-		$this->_domain = $_SERVER['HTTP_HOST'];
-		$this->_prefix = "SESSION:{$_SERVER['HTTP_HOST']}:";
+		$this->_domain = $_SERVER['SERVER_NAME'];
+		$this->_prefix = "SESSION:{$_SERVER['SERVER_NAME']}:";
 		session_set_save_handler($this, true);
 	}
 
