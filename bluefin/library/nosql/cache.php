f16ca92b740c70dcdd0787097157df574a9fc681
bluefin-framework <isme@live.com>
2016-01-03 21:21:15 +0800

    新增cache接口

---
+++ bluefin/library/nosql/cache.php
@@ -0,0 +1,14 @@
+<?php
+namespace library\nosql;
+interface cache
+{
+	public function get($key);
+	public function __get($key);
+	public function set($key, $value, $ttl=0);
+	public function __set($key, $value);
+	public function expire($key, $ttl=0);
+	public function ttl($key);
+	public function exists($key);
+	public function delete($key);
+	public function flush();
+}
