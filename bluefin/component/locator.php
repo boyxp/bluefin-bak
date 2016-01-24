a8151a6a4ccd3fbaf27d22bd813eab41c0a48639
bluefin-framework <isme@live.com>
2016-01-24 21:19:18 +0800

    增加alias、bind、has方法

--- bluefin/component/locator.php
+++ bluefin/component/locator.php
@@ -6,4 +6,7 @@ interface locator
 	public function get($name, array $params=null);
 	public function __set($name, $instance);
 	public function set($name, $instance);
+	public function alias($name, $class);
+	public function bind($name, array $params);
+	public function has($name);
 }
